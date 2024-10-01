98cd0562d74ffe27ea6079da433bcdc9e8c8f739
meshariSalhouli <mesharialhouli12@gmail.com>
2024-10-01 12:05:39 +0300

    challenge1

--- challenge1.dart
+++ challenge1.dart
@@ -3,7 +3,16 @@
  * Create a function called `printName`
  * - that prints your name on the screen.
  */
+void main(){
+printName();
+printAge(2001);
+printHello("meshari", "es");
+printMax(5, 6);
+}
 
+void printName(){
+  print(" Meshari Alhouli");
+}
 /**
  * Task 2:
  * Create a function called `printAge`
@@ -11,7 +20,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+void printAge(int birthyear){
+int current = 2024;
 
+print(current-birthyear);
+}
 /**
  * Task 3:
  * Create a function called `printHello`
@@ -22,10 +35,28 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
-
+void printHello(name,language){
+  switch(language) {
+    case "en":
+    print("Hello NAME");
+    case "es":
+    print("Hola NAME");
+    case "fr":
+    print("Bonjour NAME");
+    case "tr":
+    print("Merhaba NAME");
+  }
+}
 /**
  * Task 4:
  * Create a function called `printMax`
  * - that takes two numbers as parameters,
  * - and prints out the bigger number
  */
+void printMax(int num1 ,int num2){
+if(num1<num2){
+print( num2);
+} else(){
+print(num1);
+};
+}
